a6b258515fbfd5d725b78b4e1fdcddbe92fac417
andre custodio <andre.sc@outlook.pt>
2023-03-13 18:45:20 +0000

    fixed a bug where typing unit without space broke the code

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -1,25 +1,25 @@
 class ConverterController < ApplicationController
   def index
-    input = params[:input]&.strip # assigning the value from the params to "input" and using strip to remove spaces
-    @output = if input.present? # creates the @output variable if the input has a value
-      from, to = input.downcase.split(' in ') #splits the array in half to identify the ammount and units used ( "2 kg lbs" should be the result)
-      if from.present? && to.present? # checks if both values are present and further splits the array to get the amount and original unit
-        from_value, from_unit = from.split(' ')
-        @to_unit = to
-        case [from_unit, @to_unit] # here we start the logic, we have the units ready to be used, they are checked on the when clause, and we use float to calculate
+    input = params[:input]&.strip
+    @output, @to_unit = if input.present?
+      from, to = input.downcase.split(' in ')
+      if from.present? && to.present?
+        from_value, from_unit = from.split(/(?<=\d)(?=[a-z])/i)
+        to_unit = to.strip.downcase
+        case [from_unit, to_unit]
         when ['kg', 'lbs']
-          (from_value.to_f * 2.20462).round(2)
+          [(from_value.to_f * 2.20462).round(2), 'lbs']
         when ['lbs', 'kg']
-          (from_value.to_f / 2.20462).round(2)
+          [(from_value.to_f / 2.20462).round(2), 'kg']
         when ['m', 'ft']
-          (from_value.to_f * 3.28084).round(2)
+          [(from_value.to_f * 3.28084).round(2), 'ft']
         when ['ft', 'm']
-          (from_value.to_f / 3.28084).round(2)
+          [(from_value.to_f / 3.28084).round(2), 'm']
         else
-          'Invalid conversion units. Please type the units as following: "kg, lbs, m, ft." '
+          ['Invalid conversion units. Please type the units as following: "kg, lbs, m, ft."', nil]
         end
       else
-        'Invalid input format.'
+        ['Invalid input format.', nil]
       end
     end
   end
