ab7eb3eb9a609e69871e349b53d757bc14b19532
andre custodio <andre.sc@outlook.pt>
2023-03-13 17:27:30 +0000

    finished the logic using the search bar function. still very limited to user input but it works

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'converter#index'
 
-  resources :converter, only: [:index]
+  resources :converter, only: [:index, :evolved]
   # Defines the root path route ("/")
 end
